fc637debb57aee5abfe78ac1f00975bbe2b273bf
Dmitry Smirnov <dmsmirn08@mail.ru>
2026-04-18 18:30:46 +0300

    Добавлена подсветка кнопки Найти при наведении

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -48,6 +48,10 @@ public partial class MainWindow : Window
         SearchButton.Click += SearchButton_Click;
         SearchTextBox.KeyDown += SearchTextBox_KeyDown;
         
+        // Подсветка кнопки поиска
+        SearchButton.PointerEntered += (s, e) => SearchButton.Background = Avalonia.Media.Brushes.LightGray;
+        SearchButton.PointerExited += (s, e) => SearchButton.Background = new Avalonia.Media.SolidColorBrush(Avalonia.Media.Color.Parse("#CCCCCC"));
+        
         KeyDown += MainWindow_KeyDown;
     }
 
