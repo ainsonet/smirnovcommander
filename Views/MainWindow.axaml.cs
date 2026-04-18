33f6e609ca3e32893747f692409dd7aa84d42be0
Dmitry Smirnov <dmsmirn08@mail.ru>
2026-04-18 20:45:41 +0300

    Исправлен перехват горячих клавиш через AddHandler

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -56,8 +56,8 @@ public partial class MainWindow : Window
         RightCreateFolderButton.Click += (s, e) => { if (DataContext is MainWindowViewModel vm) vm.RightPanel.CreateFolder(); };
         RightRenameButton.Click += RightRenameButton_Click;
         
+        AddHandler(InputElement.KeyDownEvent, Window_KeyDown, RoutingStrategy.Tunnel);
         KeyDown += Window_KeyDown;
-        PreviewKeyDown += Window_KeyDown;
     }
 
     private void LeftPanelList_DoubleTapped(object? sender, TappedEventArgs e)
